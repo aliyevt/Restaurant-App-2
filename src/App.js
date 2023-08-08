e9286251d399351d1a041d5000a2b308349124d7
TURAL <shaqalmaktoum@gmail.com>
2023-08-08 23:38:01 +0400

    Restaurant-App
    This is a restaurant website

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+        <Hero />
+        <HeadlineCards />
+        <Food />
+        <Category />
+    </div>
+  );
+}
+
+export default App;
